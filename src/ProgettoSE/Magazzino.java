568f8e361fa75d4d4ec1219f58c35635e9dc3a06
Your Name <f.dagani002@studenti.unibs.it>
2023-02-23 13:43:58 +0100

    push x simo

--- src/ProgettoSE/Magazzino.java
+++ src/ProgettoSE/Magazzino.java
@@ -4,7 +4,7 @@ import ProgettoSE.Alimentari.*;
 import java.util.ArrayList;
 
 public class Magazzino {
-    private ArrayList<Bevanda> bevande;
-    private ArrayList<Extra> extras;
-    private ArrayList<Ingrediente> ingredienti;
+    private ArrayList<Alimento> bevande = new ArrayList<Bevanda>();
+    private ArrayList<Alimento> extras;
+    private ArrayList<Alimento> ingredienti;
 }
